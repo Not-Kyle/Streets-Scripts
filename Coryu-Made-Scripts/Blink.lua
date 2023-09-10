81460f3725315533ce1d5b4a5f64c087af06b8c6
CORYU <77679412+Not-Kyle@users.noreply.github.com>
2023-09-10 00:31:21 -0400

    Update Blink.lua

--- Coryu-Made-Scripts/Blink.lua
+++ Coryu-Made-Scripts/Blink.lua
@@ -21,7 +21,7 @@ if not getgenv()['CoryuBlink'] then
     
     local CoryuBlinkFile;
     makefolder('Coryu')
-    xpcall(function()
+    ypcall(function()
     	CoryuBlinkFile = readfile('Coryu/CoryuBlink.js')
     end)
     

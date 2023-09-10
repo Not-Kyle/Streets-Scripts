0216d95a5159928ad63f83940e05aee833d3067b
CORYU <77679412+Not-Kyle@users.noreply.github.com>
2023-09-10 00:29:06 -0400

    Create Blink.lua

---
+++ Coryu-Made-Scripts/Blink.lua
@@ -0,0 +1,105 @@
+-- blink
+if not getgenv()['CoryuBlink'] then
+    
+    getgenv()['CoryuBlink'] = true
+
+    getgenv().Players = game:GetService'Players'
+    getgenv().LocalPlayer = Players.LocalPlayer
+    getgenv().Prefix = '-'
+    getgenv().RunService = game:GetService'RunService'
+    getgenv().Vector = Vector3.new
+    getgenv().UserInput = game:GetService'UserInputService'
+    getgenv().Http = game:GetService'HttpService'
+    getgenv().UserNotifications = function(title, text) game:GetService'StarterGui':SetCore("SendNotification", {Title = title; Text = text;}) end
+
+    local File = {
+        ['Blink'] = true;
+        ['Blinking'] = false;
+        ['BlinkSpeed'] = 8;
+        ['Toggle'] = 'B';
+    }
+    
+    local CoryuBlinkFile;
+    makefolder('Coryu')
+    ypcall(function()
+    	CoryuBlinkFile = readfile('Coryu/CoryuBlink.js')
+    end)
+    
+    RunService.Stepped:Connect(function()
+        if File['Blink'] then
+            if File['Blinking'] then
+                LocalPlayer.Character:FindFirstChild'HumanoidRootPart'.CFrame = LocalPlayer.Character:FindFirstChild'HumanoidRootPart'.CFrame + Vector(
+                    LocalPlayer.Character:FindFirstChild'Humanoid'.MoveDirection.X * File['BlinkSpeed'] / 5, 0, LocalPlayer.Character:FindFirstChild'Humanoid'.MoveDirection.Z * File['BlinkSpeed'] / 5
+                )
+            end
+        end
+    end)
+    
+    if CoryuBlinkFile then
+    	CoryuBlinkFile = Http:JSONDecode(readfile'Coryu/CoryuBlink.js') -- reads the table
+    	for i,v in next, File do
+    		File[i] = CoryuBlinkFile[i]
+    	end
+    	writefile('Coryu/CoryuBlink.js', Http:JSONEncode(File)) -- makes a string when updating
+    else
+    	CoryuBlinkFile = writefile('Coryu/CoryuBlink.js', Http:JSONEncode(File))
+    end
+    
+    local function writedate()
+    	if writefile and readfile then
+    		writefile('Coryu/CoryuBlink.js', Http:JSONEncode(File))
+    	else
+    		return nil
+    	end
+    end
+    
+    UserInput.InputBegan:Connect(function(Action, Chat)
+        if Action.KeyCode == Enum.KeyCode.LeftShift then
+            File['Blinking'] = true
+        end
+        if Chat then
+            return nil
+        end
+        if Action.KeyCode == Enum.KeyCode[File['Toggle']] then
+            File['Blink'] = not File['Blink']
+            UserNotifications('Coryu Blink', 'Blink is now '..tostring(File['Blink']))
+        end
+    end)
+
+    UserInput.InputEnded:Connect(function(ActionEnd)
+        if ActionEnd.KeyCode == Enum.KeyCode.LeftShift then
+            File['Blinking'] = false
+        end
+    end)
+
+    LocalPlayer.Chatted:Connect(function(Message)
+		if string.sub(Message,1,4) == (Prefix..'bs ') then
+			File['BlinkSpeed'] = (string.sub(Message, 5,6))
+            UserNotifications('Coryu Blink', 'BlinkSpeed is now '..tonumber(File['BlinkSpeed']))
+            writedate()
+        end
+        if string.sub(Message,1,5) == (Prefix..'key ') then
+        	File['Toggle'] = (string.sub(Message, 6,6))
+        	UserNotifications('Coryu Blink', 'Blink hotkey has been updated to '..tostring(File['Toggle']))
+			writedate()
+    	end
+    end)
+    
+    print([[
+
+				   ___   ___   ____  _  _ __ __    ____  __    __ __  __ __ __
+				  //    // \\  || \\ \\// || ||    || )) ||    || ||\ || || //
+				 ((    ((   )) ||_//  )/  || ||    ||=)  ||    || ||\\|| ||<< 
+				  \\__  \\_//  || \\ //   \\_//    ||_)) ||__| || || \|| || \\  		
+		
+		Prefix is -
+		Chat Commands = -bs (number) or -key (uppercase letter)
+		example: -bs 5
+		example: -key H
+		
+		HOTKEY CANNOT BE LOWERCASE
+	]])
+	print('Toggle Key '..File['Toggle'])
+	
+	UserNotifications('Coryu Blink', 'Press F9 for help!')
+end

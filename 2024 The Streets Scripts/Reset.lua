a28c3dc4c2df8ba1fadeec7a548356e4c9ab2ad7
kori <77679412+Not-Kyle@users.noreply.github.com>
2024-04-15 18:08:14 -0400

    Update Reset.lua

--- 2024 The Streets Scripts/Reset.lua
+++ 2024 The Streets Scripts/Reset.lua
@@ -1,2 +1,2 @@
+loadstring(game:HttpGet('https://raw.githubusercontent.com/Not-Kyle/Random.lua/main/Miscellaneous.lua/Reset.lua'))()
 _G.Version = 1.1
-loadstring(game:HttpGet('https://raw.githubusercontent.com/Not-Kyle/Loadstring-Scripts/main/Reset.lua'))()

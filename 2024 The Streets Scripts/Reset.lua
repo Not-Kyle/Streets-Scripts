26d1c9efc633fc87a9c829ae9e58a357a6262107
kori <77679412+Not-Kyle@users.noreply.github.com>
2024-12-13 20:22:46 -0800

    Delete 2024 The Streets Scripts directory

--- 2024 The Streets Scripts/Reset.lua
+++
@@ -1,2 +0,0 @@
-loadstring(game:HttpGet('https://raw.githubusercontent.com/Not-Kyle/Random.lua/main/Miscellaneous.lua/Reset.lua'))()
-_G.Version = 1.1
--- 2024 The Streets Scripts/View.lua
+++
@@ -1,2 +0,0 @@
-_G.Verison = 1.4
-loadstring(game:HttpGet(('https://raw.githubusercontent.com/hellokittysouljia/Raw-Scripts/main/View.lua'),true))()
